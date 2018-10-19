3175c1ea75332b46af24add0952c7f783bbe0550
erezsuh <erezsuh@gmail.com>
2018-10-19 19:30:13 +0300

    Router and empty dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 
+import { Route, Switch } from 'react-router-dom';
 import MainGamePage from './main-game-page';
+import GameDashboard from './game-dashboard';
+
+
+const mainGamePage = () => (
+    <MainGamePage />
+)
+
+const gameDashboard = () => (
+    <GameDashboard />
+)
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <MainGamePage />
-      </div>
-    );
-  }
+    render() {
+        return <Switch>
+            <Route path="/" exact component={mainGamePage} />
+            <Route path="/dashboard" component={gameDashboard} />
+        </Switch>; 
+    }
 }
 
 export default App;
---
+++ frontend/src/game-dashboard.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class GameDashboard extends Component {
+    render() {
+        return <h2>Cut-it-out Dashboard</h2>;
+    }
+}
+
+export default GameDashboard;
