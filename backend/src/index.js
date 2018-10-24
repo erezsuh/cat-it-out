42d8882b01fd33ccb2ac1fed42c4ac76d35d7611
erezsuh <erezsuh@gmail.com>
2018-10-24 19:27:14 +0300

    Add start game option

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,17 @@
-var express = require('express');
+/********* Todo - Refactor this file ************/
+var express = require('express'); // Todo -  use ES6 to require/import packages
 var app = express();
 var bodyParser = require("body-parser");
 
 const GAME_STATUS = true;
-
+const DASHBOARD_PORT = 45010;
+const PLAYERS_PORT = 45011;
 var currentPlayers = [];
 var dashboardWsConnection = null;
 
 var WebSocketServer = require('ws').Server;
-wss = new WebSocketServer({port: 40510});
-wss.on('connection', function (ws) {
+dashboardWsServer = new WebSocketServer({port: DASHBOARD_PORT});
+dashboardWsServer.on('connection', function (ws) {
     ws.on('message', function (message) {
         console.log('received: %s', message);
     });
@@ -26,6 +28,21 @@ wss.on('connection', function (ws) {
     // }
 
     dashboardWsConnection = ws;
+});
+
+playersWsServer = new WebSocketServer({port: PLAYERS_PORT});
+playersWsServer.on('connection', function (ws, req) {
+    console.log(`req:${req}`);
+    ws.on('message', function (message,) {
+        //Todo - Handle different messages
+        console.log('received: %s', message);
+        currentPlayers.push(message);
+    });
+
+    ws.on('close', function(id) {
+        //We don't remove the player
+        console.log('Connection ' +  id + ' has left the server');
+    });
 });
 
 app.use(bodyParser.json());
@@ -51,8 +68,16 @@ app.post('/api/newPlayer', function (req, res) {
     console.log('new Player!!' + req.body.playerName);
     currentPlayers.push(req.body.playerName)
     res.send();
-    dashboardWsConnection.send(JSON.stringify(currentPlayers));
+    //Ugly :)
+    dashboardWsConnection && dashboardWsConnection.send(JSON.stringify(currentPlayers));
 });
+
+app.post('/api/startGame', function (req, res) {
+    console.log(`Game started by ${req.body.playerName}`);
+    currentPlayers.push(req.body.playerName)
+    res.send();
+});
+
 
 app.listen(3001, function () {
    console.log('Example app listening on port 3001!')
--- frontend/src/ActiveGame.jsx
+++ frontend/src/ActiveGame.jsx
@@ -1,9 +1,39 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
 
 class ActiveGame extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            gameStarted: false
+        };
+
+        this.startGame = this.startGame.bind(this);
+    }
+    
+    startGame() {
+        axios.post('/api/startGame', {
+            playerName: this.props.playerName
+        })
+            .then((result) => {
+                this.setState({
+                    gameStarted: result
+                });
+            });
+    }
+
     render() {
-        return <h1>Waiting for more players...</h1>;
+        return <div>
+            {this.state.gameStarted ? <h1>Game Started by ???</h1> : <button onClick={this.startGame}>Start game</button>}
+        </div>;
+        
     }
 }
+
+ActiveGame.propTypes = {
+    playerName: PropTypes.string.isRequired
+};
 
 export default ActiveGame;
